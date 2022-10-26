88108fb097164f8805bfe64384a7b348c05e9693
Denis Troenko <wcproof-grief@yandex.ru>
2022-10-26 11:10:18 +0300

    added fields in session (custom/views). edited templates

--- custom/view.py
+++ custom/view.py
@@ -16,6 +16,9 @@ def mapping_view(app: object, site :object):
         session['phone'] = feedback_form.phone.data
         session['email'] = feedback_form.email.data
         session['message'] = feedback_form.message.data
+        session['page_url'] = feedback_form.page_url.data
+        session['page_name'] = feedback_form.page_name.data
+        session['choice'] = feedback_form.choice.data
 
         # ВСТАВИТЬ СЮДА ОБРАБОТКУ ФОРМЫ
 
